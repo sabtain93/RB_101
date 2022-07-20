ec354a1928084f031399796086ef3e7a7f52b916
Muhammad Sabtain <sabtain40@gmail.com>
2022-07-20 20:48:26 +0500

    bonus features added to the program

--- Lesson_2/calculator.rb
+++ Lesson_2/calculator.rb
@@ -2,38 +2,50 @@
 # asks user for two numbers
 # asks for the type of operation to perform : add, subtract, multiply, divide
 # display results
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(input)
   puts "=> #{input}"
 end
 
-def valid_number(num) # validation of the number input by user
-  num.to_i != 0
+def float?(num)
+  num.to_f.to_s == num
+ end 
+
+def integer?(num) # validation of the number input by user
+  num.to_i.to_s == num
 end
 
+def number?(num)
+  integer?(num) || float?(num)
+ end
+  
 def valid_operation(op) # validation of the operator chosen by user
   %w(1 2 3 4).include?(op)
 end
 
 def op_sel(op) # to define the opeartion
-  case op
-  when '1'
-    'Addition'
-  when '2'
-    'subtraction'
-  when '3'
-    'Multiplication'
-  when '4'
-    'Division'
-  end
+  answer = case op
+           when '1'
+            'Addition'
+           when '2'
+            'subtraction'
+           when '3'
+            'Multiplication'
+           when '4'
+            'Division'
+          end
+  answer      
 end
 
-prompt('welcome to the calculator, please enter your name')
+prompt(MESSAGES['welcome'])
 user = ''
 
 loop do # to check if user entered name or not
   user = gets.chomp
   if user.empty?()
-    prompt('enter your name')
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
@@ -44,25 +56,26 @@ loop do # main loop of the calculator
   number1 = ''
 
   loop do # loop to validate first number
-    prompt('Enter the first number: ')
+    prompt(MESSAGES['1_st'])
     number1 = Kernel.gets().chomp()
-    if valid_number(number1)
+    if number?(number1)
       break
     else
-      prompt('Enter a valid number')
+      prompt(MESSAGES['valid_number'])
     end
   end
   number2 = ''
   loop do # loop to validate second number
-    prompt('Enter the second number: ')
+    prompt(MESSAGES['2_nd'])
     number2 = Kernel.gets().chomp()
 
-    if valid_number(number2)
+    if number?(number2)
       break
     else
-      prompt('Enter a valid number')
+      prompt(MESSAGES['valid_number'])
     end
   end
+  
   prompt_operator = <<-MSG
     Select the operation to be performed
       1) Add
@@ -70,6 +83,7 @@ loop do # main loop of the calculator
       3) Multiply
       4) Divide
   MSG
+  
 
   prompt(prompt_operator)
   operator = ''
@@ -79,7 +93,7 @@ loop do # main loop of the calculator
     if valid_operation(operator)
       break
     else
-      prompt('Enter a valid operation')
+      prompt(MESSAGES['valid_oper'])
     end
   end
 
@@ -97,8 +111,8 @@ loop do # main loop of the calculator
   end
   prompt("The result is : #{answer}")
 
-  prompt("Would yoy like to perform another operation? (Y/N): ")
+  prompt(MESSAGES['repeat'])
   reply = gets.chomp
   break unless reply.downcase.start_with?('y')
 end
-prompt('Thank you for using the calculator')
+prompt(MESSAGES['goodbye'])
