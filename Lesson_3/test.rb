571314ba2f202f5174da46cfe812dd9c133d7640
Muhammad Sabtain <sabtain40@gmail.com>
2022-09-02 09:47:58 +0500

    updating test file

--- Lesson_3/test.rb
+++ Lesson_3/test.rb
@@ -1,30 +1,5 @@
-# PROBLEM:
+def some_method(a)
+  a == 1 || a == 2
+end
 
-# Given a string, write a method `palindrome_substrings` which returns
-# all the substrings from a given string which are palindromes. Consider
-# palindrome words case sensitive.
-
-# Test cases:
-
-# palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
-# palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
-# palindrome_substrings("palindrome") == []
-# palindrome_substrings("") == []
-
-
-
-# input: String
-# output: array (contain string objects which are palindrome)
-# rules: 
-#       
-#       Explicit requirements:
-#               - every plaindrome substring is to be assigned into an array
-#               - palindrome word are case sensitive('aba' is palindrome, 'abA' is not)
-#       
-#       Implicit requirements
-#               - if no palindrome word is detected return an empty array
-#               - if an empty string is passed return an empty array.
-#
-#
-#
-#
+p some_method(3)
